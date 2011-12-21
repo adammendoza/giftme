a63d8ac3dbc93cbbe7bd8fe21617f13f39718a38
Amy Palamountain <amy@palamounta.in>
2011-12-22 09:23:49 +1300

    Added new CSS for Gift row.

--- source/Ammeep.GiftRegister.Web/Controllers/WishlistController.cs
+++ source/Ammeep.GiftRegister.Web/Controllers/WishlistController.cs
@@ -16,7 +16,7 @@ namespace Ammeep.GiftRegister.Web.Controllers
             WishlistPageModel pageModel = new WishlistPageModel();
             pageModel.WishlistTitle = "The mightly list";
             GiftWish wish1 = new GiftWish();
-            wish1.ImageLocation = new Uri("http://baconmockup.com/300/200");
+            wish1.ImageLocation = new Uri("http://baconmockup.com/170/165");
             wish1.ItemName = "Item Name";
             wish1.Description = "Shoulder hamburger frankfurter, biltong tail shankle drumstick prosciutto short ribs pastrami. Boudin kielbasa shank cow. Andouille turducken filet mignon, pancetta capicola beef ribs pork meatloaf. Shoulder corned beef ball tip jerky pig. Short ribs pork loin sirloin pig, tail meatloaf turducken swine. Flank tail cow chicken filet mignon, capicola andouille biltong pastrami frankfurter. Meatball jerky shankle, jowl pork chop prosciutto tongue andouille turducken tail rump.";
             wish1.QuantityRequired = 2;
