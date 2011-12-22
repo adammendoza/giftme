e5fe683919e6342130a2404c290aa3063c60ee50
Amy Palamountain <amy@palamounta.in>
2011-12-23 11:19:43 +1300

    Fixed css error on first registry item in the row

--- source/Ammeep.GiftRegister.Web/Models/WishlistPageModel.cs
+++ source/Ammeep.GiftRegister.Web/Models/WishlistPageModel.cs
@@ -7,4 +7,10 @@ namespace Ammeep.GiftRegister.Web.Models
         public string WishlistTitle { get; set; }
         public IEnumerable<GiftWish> GiftWishes { get; set; }
     }
+
+    public class GiftRow
+    {
+        public bool IsFirst { get; set; }
+        public GiftWish Item { get; set; }
+    }
 }
