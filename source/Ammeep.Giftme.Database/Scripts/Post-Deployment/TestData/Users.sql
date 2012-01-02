194101f9132cad29709ca5bf88eace5b905de930
Amy Palamountain <amy@palamounta.in>
2012-01-03 00:11:33 +1300

    Updated User Test Data to reflect new schema

--- source/Ammeep.Giftme.Database/Scripts/Post-Deployment/TestData/Users.sql
+++ source/Ammeep.Giftme.Database/Scripts/Post-Deployment/TestData/Users.sql
@@ -1,6 +1,6 @@
 ﻿
 SET IDENTITY_INSERT [Users] ON
-INSERT [Users] ([UserId], [UserName], [Password]) VALUES (1, N'Lydia74', N'SGO2X8MAT0DUQPT1V')
-INSERT [Users] ([UserId], [UserName], [Password]) VALUES (2, N'Danny711', N'K90IX')
-INSERT [Users] ([UserId], [UserName], [Password]) VALUES (3, N'Gavin61', N'O')
+INSERT [Users] ([UserId], [UserName], [Email],[FirstName],[LastName],[PasswordHash],[PasswordSalt]) VALUES (1, N'Lydia74',N'test@test.com',N'Test',N'Test', N'SGO2X8MAT0DUQPT1V', N'SGO2X8MAT0DUQPT1V')
+INSERT [Users] ([UserId], [UserName], [Email],[FirstName],[LastName],[PasswordHash],[PasswordSalt]) VALUES (2, N'Danny711',N'test@test.com',N'Test',N'Test', N'SGO2X8MAT0DUQPT1V', N'SGO2X8MAT0DUQPT1V')
+INSERT [Users] ([UserId], [UserName], [Email],[FirstName],[LastName],[PasswordHash],[PasswordSalt]) VALUES (3, N'Gavin61',N'test@test.com',N'Test',N'Test', N'SGO2X8MAT0DUQPT1V', N'SGO2X8MAT0DUQPT1V')
 SET IDENTITY_INSERT [Users] OFF
