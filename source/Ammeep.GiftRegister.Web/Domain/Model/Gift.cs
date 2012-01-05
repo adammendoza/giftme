25fea77c50ea60132f101fce032eb0c2ea368ee3
Amy Palamountain <amy@palamounta.in>
2012-01-06 11:27:18 +1300

    added editorfor and display for templates for gift

--- source/Ammeep.GiftRegister.Web/Domain/Model/Gift.cs
+++ source/Ammeep.GiftRegister.Web/Domain/Model/Gift.cs
@@ -6,20 +6,25 @@ namespace Ammeep.GiftRegister.Web.Domain.Model
     {
         [Display(Name = "Image Url")]
         [UIHint("Image")]
+        [Required]
         public string ImageLocation { get; set; }
 
         [Display(Name = "Item Name")]
         [UIHint("ItemName")]
+        [Required]
         public string Name { get; set; }
 
         [UIHint("Description")]
+        [Required]
         public string Description { get; set; }
 
         [UIHint("Website")]
+        [Required]
         public string Website { get; set; }
 
         [Display(Name = "Suggested Store(s)")]
         [UIHint("Stores")]
+        [Required]
         public string SuggestedStores { get; set; }
 
         [UIHint("YesNo")]
@@ -28,14 +33,17 @@ namespace Ammeep.GiftRegister.Web.Domain.Model
 
         [Display(Name = "Quantity")]
         [UIHint("Quantity")]
+        [Required]
         public int QuantityRequired { get; set; }
 
         [UIHint("Cost")]
         [Display(Name = "RRP")]
+        [Required]
         public decimal RetailPrice { get; set; }
         public bool IsPurchased { get; set; }
 
         [UIHint("Category")]
+        [Required]
         public string Category { get; set; }
         public int GiftId { get; set; }
     }
