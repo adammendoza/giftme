b58c2067175d93667bb6a87f12c72273e4a6d21c
Amy Palamountain <amy@palamounta.in>
2012-01-06 16:51:46 +1300

    Html encoded the JavaScript strings on the edit page

---
+++ source/Ammeep.GiftRegister.Web/HtmlExtentions.cs
@@ -0,0 +1,13 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace Ammeep.GiftRegister.Web
+{
+    public static class HtmlExtensions
+    {
+        public static IHtmlString JavaScriptEncode(this HtmlHelper html, string item)
+        {
+            return new HtmlString(HttpUtility.JavaScriptStringEncode(item));
+        }
+    }
+}
